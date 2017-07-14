14c86a8b126f82cc7679b7222f25be95a4304df0
Andrey Balandin <andrvb@gmail.com>
2017-07-14 21:08:59 +0300

    fix urlpatterns deprecations in django 1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django_openid_provider",
-    version = "0.4.5",
+    version = "0.4.6",
     author = "Roman Barczyński",
     description = "An OpenID provider for your django.contrib.auth accounts",
     long_description = open("README.txt", "rb").read().decode("utf-8"),
